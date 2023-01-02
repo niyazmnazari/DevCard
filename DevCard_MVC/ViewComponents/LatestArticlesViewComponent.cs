a09ccbea35dc5ad4c4b114ef97da32e9fb8941c1
niyazmnazari <niyaz.mnazari@gmail.com>
2023-01-02 09:33:45 +0330

    Footer and Header moved to Partial

--- DevCard_MVC/ViewComponents/LatestArticlesViewComponent.cs
+++ DevCard_MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -8,13 +8,13 @@ namespace DevCard_MVC.ViewComponents
 	{
 		public IViewComponentResult Invoke()
 		{
-			var projects = new List<Article>
+			var articles = new List<Article>
 			{
 				new Article(1,"Top 3 JavaScript Frameworks","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean\r\n\t\t\t\t\t\t\tcommodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis\r\n\t\t\t\t\t\t\tparturient...","blog-post-thumb-card-1.jpg" ),
 				new Article(2,"About Remote Working","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean\r\n\t\t\t\t\t\t\tcommodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis\r\n\t\t\t\t\t\t\tparturient...","blog-post-thumb-card-2.jpg" ),
 				new Article(3,"A Guide to Becoming a Full-Stack Developer","Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean\r\n\t\t\t\t\t\t\tcommodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis\r\n\t\t\t\t\t\t\tparturient...","blog-post-thumb-card-3.jpg"),
 			};
-			return View("_LatestArticles");
+			return View("_LatestArticles", articles);
 		}
 	}
 }
--- DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -9,12 +9,12 @@ namespace DevCard_MVC.ViewComponents
 		public IViewComponentResult Invoke() {
 			var projects = new List<Project>
 			{
-				new Project(1,"Project Heading","Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes.","~/assets/images/project/project-1.jpg","Client: Google"),
-				new Project(2,"Project Heading","Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes.","~/assets/images/project/project-2.jpg","Client: Google"),
-				new Project(3,"Project Heading","Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes.","~/assets/images/project/project-3.jpg","Client: Google"),
-				new Project(4,"Project Heading","Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes.","~/assets/images/project/project-4.jpg","Client: Google"),
+				new Project(1,"Project Heading","Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes.","project-1.jpg","Client: Google"),
+				new Project(2,"Project Heading","Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes.","project-2.jpg","Client: Google"),
+				new Project(3,"Project Heading","Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes.","project-3.jpg","Client: Google"),
+				new Project(4,"Project Heading","Project intro lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes.","project-4.jpg","Client: Google"),
 			};
-			return View("_Projects");
+			return View("_Projects", projects);
 		}
 	}
 }
